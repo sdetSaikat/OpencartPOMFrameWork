7b7ad28866a4a1a9ca6ec06f9f49a02e969021e8
sdetSaikat <talktosaikat9@gmail.com>
2025-12-09 15:08:48 +0530

    Create TimeUtil.java

---
+++ src/main/java/com/qa/opencart/utilities/TimeUtil.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.utilities;
+
+public class TimeUtil {
+
+	
+	private static final int MAX_TIME = 15;
+	
+	public  static int waitForMaxTime() {
+		return MAX_TIME;
+	}
+	
+}
