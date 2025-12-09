b1184c95927fa36a6798298cc4c31a3badfc45d6
Saikat Maity <saikatmaity@Saikats-MacBook-Pro.local>
2025-12-09 15:38:52 +0530

    saving the latest pull

--- src/main/java/com/qa/opencart/utilities/TimeUtil.java
+++ src/main/java/com/qa/opencart/utilities/TimeUtil.java
@@ -2,14 +2,13 @@ package com.qa.opencart.utilities;
 
 public class TimeUtil {
 
-	
+
 	private static final int SHORT_TIME = 10;
 	
 	public  static int waitForShortTime() {
 		return SHORT_TIME;
 	}
 	
-}
 	private static final int MAX_TIME = 15;
 	
 	public  static int waitForMaxTime() {
