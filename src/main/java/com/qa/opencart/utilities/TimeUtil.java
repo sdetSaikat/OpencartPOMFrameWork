2ecc174aa9a0f7a3c63e67ec7849125a7bcabfba
Saikat Maity <saikatmaity@Saikats-MacBook-Pro.local>
2025-12-09 15:11:26 +0530

    adding TimeUtil

---
+++ src/main/java/com/qa/opencart/utilities/TimeUtil.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.utilities;
+
+public class TimeUtil {
+
+	
+	private static final int SHORT_TIME = 5;
+	
+	public  static int waitForShortTime() {
+		return SHORT_TIME;
+	}
+	
+}
